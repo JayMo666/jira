0b9b7103f075d025066ad3c4bcb696227437dea5
JayMo666 <mojuegui@gmail.com>
2021-06-21 09:44:50 +0800

    feat: login接口

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { ProjectListScreen } from "screens/project-list";
 // import { TsReactTest } from "./test/try-use-array";
+import { LoginScreen } from "screens/login";
 
 function App() {
   return (
     <div className="App">
-      <ProjectListScreen></ProjectListScreen>
-      {/* <TsReactTest></TsReactTest> */}
+      {/* <ProjectListScreen></ProjectListScreen>
+      <TsReactTest></TsReactTest> */}
+      <LoginScreen />
     </div>
   );
 }
