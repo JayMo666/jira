8a9e0c590327eb714197eab88ee10c3f94c52e52
JayMo666 <mojuegui@gmail.com>
2021-06-21 14:23:30 +0800

    添加登录注册系统

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import React from "react";
-import { ProjectListScreen } from "screens/project-list";
+import { AuthenticatedApp } from "authenticated-app";
+import { useAuth } from "context/auth-context";
+import { UnauthenticatedApp } from "unauthenticated-app";
 // import { TsReactTest } from "./test/try-use-array";
-import { LoginScreen } from "screens/login";
-
 function App() {
+  const { user } = useAuth();
   return (
     <div className="App">
-      {/* <ProjectListScreen></ProjectListScreen>
-      <TsReactTest></TsReactTest> */}
-      <LoginScreen />
+      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
     </div>
   );
 }
